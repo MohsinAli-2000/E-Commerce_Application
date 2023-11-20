47a3c67aed2cfe955331943355d12704c70d75c4
Mohsin Ali <141604446+MohsinAli-2000@users.noreply.github.com>
2023-11-20 16:24:59 +0500

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-import productModel from "../models/productModel.js";
+import productModel from "../models/ProductModel.js";
 import CategoryModel from "../models/CategoryModel.js";
 import orderModel from "../models/orderModel.js";
 import fs from "fs";
