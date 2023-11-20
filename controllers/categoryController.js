f5b7f0d1242e0a91b12880d52f4b266ea4a562ae
Mohsin Ali <141604446+MohsinAli-2000@users.noreply.github.com>
2023-11-20 16:22:23 +0500

    Update categoryController.js

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,4 @@
-import categoryModel from "../models/categoryModel.js";
+import categoryModel from "../models/CategoryModel.js";
 import slugify from "slugify";
 export const createCategoryController = async (req, res) => {
   try {
